c0664be30b3d53ab374f5131c19cca4222ac001b
Olivier THEBAULT <olivier@othebau.lt>
2021-06-05 08:04:19 +0200

    Extraction de la récupération du device

--- Player2/fr/ipmfrance/gui/MainForm.cs
+++ Player2/fr/ipmfrance/gui/MainForm.cs
@@ -11,30 +11,21 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Diagnostics;
-using System.Runtime.InteropServices.ComTypes;
-using System.Runtime.InteropServices;
 using fr.ipmfrance.webcam;
-using fr.ipmfrance.webcam.com;
+using fr.ipmfrance.webcam.tools;
 
 namespace fr.ipmfrance.gui
 {
     public partial class MainForm : Form
     {
         private Stopwatch stopWatch = null;
-        private List<webcam.FilterInfo> devices;
-        private webcam.FilterInfo theDevice;
+        private static webcam.FilterInfo theDevice;
 
         public MainForm()
         {
             InitializeComponent();
-
-            devices = CollectFilters(FilterCategory.VideoInputDevice);
-
-            devices.ForEach(delegate (webcam.FilterInfo filter)
-            {
-                if (filter.MonikerString.Contains("pnp")) theDevice = filter;
-            });
-        }
+            theDevice = WebcamHelper.FindDevice();
+      }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -133,146 +124,6 @@ namespace fr.ipmfrance.gui
             }
         }
 
-        private List<webcam.FilterInfo> CollectFilters(Guid category)
-        {
-            object comObj = null;
-            ICreateDevEnum enumDev = null;
-            IEnumMoniker enumMon = null;
-            IMoniker[] devMon = new IMoniker[1];
-            int hr;
-            List<webcam.FilterInfo> result = new List<webcam.FilterInfo>();
 
-            try
-            {
-                Type srvType = Type.GetTypeFromCLSID(Clsid.SystemDeviceEnum);
-                if (srvType == null)
-                {
-                    Debug.WriteLine("Failed creating device enumerator");
-                    throw new ApplicationException("Failed creating device enumerator");
-                }
-
-                comObj = Activator.CreateInstance(srvType);
-                enumDev = (ICreateDevEnum)comObj;
-
-                hr = enumDev.CreateClassEnumerator(ref category, out enumMon, 0);
-                if (hr != 0)
-                {
-
-                    Debug.WriteLine("No devices of the category");
-                    throw new ApplicationException("No devices of the category");
-                }
-
-                IntPtr n = IntPtr.Zero;
-                while (true)
-                {
-                    hr = enumMon.Next(1, devMon, n);
-                    if ((hr != 0) || (devMon[0] == null))
-                        break;
-
-                    webcam.FilterInfo filter = new webcam.FilterInfo(devMon[0]);
-                    result.Add(filter);
-
-                    Marshal.ReleaseComObject(devMon[0]);
-                    devMon[0] = null;
-                }
-
-                result.Sort();
-            }
-            catch
-            {
-            }
-            finally
-            {
-                enumDev = null;
-                if (comObj != null)
-                {
-                    Marshal.ReleaseComObject(comObj);
-                    comObj = null;
-                }
-                if (enumMon != null)
-                {
-                    Marshal.ReleaseComObject(enumMon);
-                    enumMon = null;
-                }
-                if (devMon[0] != null)
-                {
-                    Marshal.ReleaseComObject(devMon[0]);
-                    devMon[0] = null;
-                }
-            }
-            return result;
-        }
-
-        /*private void EnumeratedSupportedFrameSizes( VideoCaptureDevice videoDevice )
-        {
-            this.Cursor = Cursors.WaitCursor;
-
-            videoResolutionsCombo.Items.Clear( );
-            videoInputsCombo.Items.Clear( );
-
-            videoCapabilitiesDictionary.Clear( );
-
-            try
-            {
-                // collect video capabilities
-                VideoCapabilities[] videoCapabilities = videoDevice.VideoCapabilities;
-                int videoResolutionIndex = 0;
-
-                foreach ( VideoCapabilities capabilty in videoCapabilities )
-                {
-                    string item = string.Format(
-                        "{0} x {1}", capabilty.FrameSize.Width, capabilty.FrameSize.Height );
-
-                    if ( !videoResolutionsCombo.Items.Contains( item ) )
-                    {
-                        if ( captureSize == capabilty.FrameSize )
-                        {
-                            videoResolutionIndex = videoResolutionsCombo.Items.Count;
-                        }
-
-                        videoResolutionsCombo.Items.Add( item );
-                    }
-
-                    if ( !videoCapabilitiesDictionary.ContainsKey( item ) )
-                    {
-                        videoCapabilitiesDictionary.Add( item, capabilty );
-                    }
-                }
-
-                if ( videoCapabilities.Length == 0 )
-                {
-                    videoResolutionsCombo.Items.Add( "Not supported" );
-                }
-
-                videoResolutionsCombo.SelectedIndex = videoResolutionIndex;
-
-                // get video inputs
-                availableVideoInputs = videoDevice.AvailableCrossbarVideoInputs;
-                int videoInputIndex = 0;
-
-                foreach ( VideoInput input in availableVideoInputs )
-                {
-                    string item = string.Format( "{0}: {1}", input.Index, input.Type );
-
-                    if ( ( input.Index == videoInput.Index ) && ( input.Type == videoInput.Type ) )
-                    {
-                        videoInputIndex = videoInputsCombo.Items.Count;
-                    }
-
-                    videoInputsCombo.Items.Add( item );
-                }
-
-                if ( availableVideoInputs.Length == 0 )
-                {
-                    videoInputsCombo.Items.Add( "Not supported" );
-                }
-
-                videoInputsCombo.SelectedIndex = videoInputIndex;
-            }
-            finally
-            {
-                this.Cursor = Cursors.Default;
-            }
-        }*/
     }
 }
---
+++ Player2/fr/ipmfrance/webcam/tools/WebcamHelper.cs
@@ -0,0 +1,104 @@
+﻿using fr.ipmfrance.webcam.com;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace fr.ipmfrance.webcam.tools
+{
+    public class WebcamHelper
+    {
+
+        private static List<FilterInfo> devices;
+        private static FilterInfo theDevice;
+
+        public static FilterInfo FindDevice()
+        {
+            devices = CollectFilters(FilterCategory.VideoInputDevice);
+
+            devices.ForEach(delegate (FilterInfo filter)
+            {
+                if (filter.MonikerString.Contains("pnp"))
+                {
+                    theDevice = filter;
+                  
+                }
+            });
+            return theDevice;
+        }
+
+
+        private static List<webcam.FilterInfo> CollectFilters(Guid category)
+        {
+            object comObj = null;
+            ICreateDevEnum enumDev = null;
+            IEnumMoniker enumMon = null;
+            IMoniker[] devMon = new IMoniker[1];
+            int hr;
+            List<webcam.FilterInfo> result = new List<webcam.FilterInfo>();
+
+            try
+            {
+                Type srvType = Type.GetTypeFromCLSID(Clsid.SystemDeviceEnum);
+                if (srvType == null)
+                {
+                    Debug.WriteLine("Failed creating device enumerator");
+                    throw new ApplicationException("Failed creating device enumerator");
+                }
+
+                comObj = Activator.CreateInstance(srvType);
+                enumDev = (ICreateDevEnum)comObj;
+
+                hr = enumDev.CreateClassEnumerator(ref category, out enumMon, 0);
+                if (hr != 0)
+                {
+
+                    Debug.WriteLine("No devices of the category");
+                    throw new ApplicationException("No devices of the category");
+                }
+
+                IntPtr n = IntPtr.Zero;
+                while (true)
+                {
+                    hr = enumMon.Next(1, devMon, n);
+                    if ((hr != 0) || (devMon[0] == null))
+                        break;
+
+                    webcam.FilterInfo filter = new webcam.FilterInfo(devMon[0]);
+                    result.Add(filter);
+
+                    Marshal.ReleaseComObject(devMon[0]);
+                    devMon[0] = null;
+                }
+
+                result.Sort();
+            }
+            catch
+            {
+            }
+            finally
+            {
+                enumDev = null;
+                if (comObj != null)
+                {
+                    Marshal.ReleaseComObject(comObj);
+                    comObj = null;
+                }
+                if (enumMon != null)
+                {
+                    Marshal.ReleaseComObject(enumMon);
+                    enumMon = null;
+                }
+                if (devMon[0] != null)
+                {
+                    Marshal.ReleaseComObject(devMon[0]);
+                    devMon[0] = null;
+                }
+            }
+            return result;
+        }
+
+    }
+}
