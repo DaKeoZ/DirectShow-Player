e19dac016b83cf2ba6232866536a235b71588c34
Olivier THEBAULT <olivier@othebau.lt>
2021-06-05 10:12:44 +0200

    Externalisation de grabber

---
+++ Player2/fr/ipmfrance/webcam/Grabber.cs
@@ -0,0 +1,73 @@
+﻿using fr.ipmfrance.webcam.com;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace fr.ipmfrance.webcam
+{
+    public class Grabber : ISampleGrabberCB
+    {
+        private VideoCaptureDevice parent;
+        private int width, height;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public Grabber(VideoCaptureDevice parent)
+        {
+            this.parent = parent;
+        }
+
+        public int SampleCB(double sampleTime, IntPtr sample)
+        {
+            return 0;
+        }
+
+        public int BufferCB(double sampleTime, IntPtr buffer, int bufferLen)
+        {
+            if (parent.isNewFrameExists())
+            {
+                System.Drawing.Bitmap image = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+                BitmapData imageData = image.LockBits(
+                    new Rectangle(0, 0, width, height),
+                    ImageLockMode.ReadWrite,
+                    PixelFormat.Format24bppRgb);
+
+                int srcStride = imageData.Stride;
+                int dstStride = imageData.Stride;
+
+                unsafe
+                {
+                    byte* dst = (byte*)imageData.Scan0.ToPointer() + dstStride * (height - 1);
+                    byte* src = (byte*)buffer.ToPointer();
+
+                    for (int y = 0; y < height; y++)
+                    {
+                        Win32.memcpy(dst, src, srcStride);
+                        dst -= dstStride;
+                        src += srcStride;
+                    }
+                }
+
+                image.UnlockBits(imageData);
+                parent.OnNewFrame(image);
+
+                image.Dispose();
+            }
+
+            return 0;
+        }
+    }
+
+}
--- Player2/fr/ipmfrance/webcam/VideoCaptureDevice.cs
+++ Player2/fr/ipmfrance/webcam/VideoCaptureDevice.cs
@@ -21,8 +21,6 @@ namespace fr.ipmfrance.webcam
         private VideoInput crossbarVideoInput = VideoInput.Default;
         private static Dictionary<string, VideoInput[]> cacheCrossbarVideoInputs = new Dictionary<string, VideoInput[]>();
 
-        public event NewFrameEventHandler NewFrame;
-
         public event VideoSourceErrorEventHandler VideoSourceError;
 
         public event PlayingFinishedEventHandler PlayingFinished;
@@ -193,11 +191,7 @@ namespace fr.ipmfrance.webcam
                 graph.AddFilter(sourceBase, "source");
                 graph.AddFilter(videoGrabberBase, "grabber_video");
 
-                AMMediaType mediaType = new AMMediaType();
-                mediaType.MajorType = MediaType.Video;
-                mediaType.SubType = MediaSubType.RGB24;
-
-                videoSampleGrabber.SetMediaType(mediaType);
+                AMMediaType mediaType = SetVideoRGB24(videoSampleGrabber);
 
                 if (videoControl != null)
                 {
@@ -306,76 +300,32 @@ namespace fr.ipmfrance.webcam
             }
         }
 
-        private void OnNewFrame(Bitmap image)
+        private static AMMediaType SetVideoRGB24(ISampleGrabber videoSampleGrabber)
+        {
+            AMMediaType mediaType = new AMMediaType();
+            mediaType.MajorType = MediaType.Video;
+            mediaType.SubType = MediaSubType.RGB24;
+            videoSampleGrabber.SetMediaType(mediaType);
+            return mediaType;
+        }
+
+        public event NewFrameEventHandler NewFrame;
+
+        public bool isNewFrameExists()
+        {
+            return (NewFrame != null);
+        }
+
+        public void OnNewFrame(Bitmap image)
         {
             framesReceived++;
             bytesReceived += image.Width * image.Height * (Bitmap.GetPixelFormatSize(image.PixelFormat) >> 3);
 
             if ((!stopEvent.WaitOne(0, false)) && (NewFrame != null))
+            {
                 NewFrame(this, new NewFrameEventArgs(image));
-        }
-
-        private class Grabber : ISampleGrabberCB
-        {
-            private VideoCaptureDevice parent;
-            private int width, height;
-
-            public int Width
-            {
-                get { return width; }
-                set { width = value; }
-            }
-            public int Height
-            {
-                get { return height; }
-                set { height = value; }
-            }
-
-            public Grabber(VideoCaptureDevice parent)
-            {
-                this.parent = parent;
-            }
-
-            public int SampleCB(double sampleTime, IntPtr sample)
-            {
-                return 0;
-            }
-
-            public int BufferCB(double sampleTime, IntPtr buffer, int bufferLen)
-            {
-                if (parent.NewFrame != null)
-                {
-                    System.Drawing.Bitmap image = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-
-                    BitmapData imageData = image.LockBits(
-                        new Rectangle(0, 0, width, height),
-                        ImageLockMode.ReadWrite,
-                        PixelFormat.Format24bppRgb);
-
-                    int srcStride = imageData.Stride;
-                    int dstStride = imageData.Stride;
-
-                    unsafe
-                    {
-                        byte* dst = (byte*)imageData.Scan0.ToPointer() + dstStride * (height - 1);
-                        byte* src = (byte*)buffer.ToPointer();
-
-                        for (int y = 0; y < height; y++)
-                        {
-                            Win32.memcpy(dst, src, srcStride);
-                            dst -= dstStride;
-                            src += srcStride;
-                        }
-                    }
-
-                    image.UnlockBits(imageData);
-                    parent.OnNewFrame(image);
-
-                    image.Dispose();
-                }
-
-                return 0;
             }
         }
+
     }
 }
