cf1041ef376b4baaba85c9bc4d561c050fc5b8bd
Olivier THEBAULT <olivier@othebau.lt>
2021-06-05 09:54:59 +0200

    Désactivation crossbar nettoyage

--- Player2/fr/ipmfrance/webcam/VideoCaptureDevice.cs
+++ Player2/fr/ipmfrance/webcam/VideoCaptureDevice.cs
@@ -16,10 +16,8 @@ namespace fr.ipmfrance.webcam
         private long bytesReceived;
         private Thread thread = null;
         private ManualResetEvent stopEvent = null;
-        private bool needToSetVideoInput = false;
         private object sourceObject = null;
         private object sync = new object();
-        //private bool? isCrossbarAvailable = null;
         private VideoInput crossbarVideoInput = VideoInput.Default;
         private static Dictionary<string, VideoInput[]> cacheCrossbarVideoInputs = new Dictionary<string, VideoInput[]>();
 
@@ -35,7 +33,6 @@ namespace fr.ipmfrance.webcam
             set
             {
                 deviceMoniker = value;
-            //    isCrossbarAvailable = null;
             }
         }
 
@@ -90,8 +87,6 @@ namespace fr.ipmfrance.webcam
 
                 framesReceived = 0;
                 bytesReceived = 0;
-         //       isCrossbarAvailable = null;
-                needToSetVideoInput = true;
 
                 stopEvent = new ManualResetEvent(false);
 
@@ -164,8 +159,6 @@ namespace fr.ipmfrance.webcam
             IAMVideoControl videoControl = null;
             IMediaEventEx mediaEvent = null;
             IPin pinStillImage = null;
-   //         IAMCrossbar crossbar = null;
-
             try
             {
                 captureGraphObject = ComFactory.Create(Clsid.CaptureGraphBuilder2);
@@ -205,13 +198,6 @@ namespace fr.ipmfrance.webcam
                 mediaType.SubType = MediaSubType.RGB24;
 
                 videoSampleGrabber.SetMediaType(mediaType);
-
-                //captureGraph.FindInterface(FindDirection.UpstreamOnly, Guid.Empty, sourceBase, typeof(IAMCrossbar).GUID, out crossbarObject);
-                //if (crossbarObject != null)
-                //{
-                //    crossbar = (IAMCrossbar)crossbarObject;
-                //}
-        //        isCrossbarAvailable = (crossbar != null);
 
                 if (videoControl != null)
                 {
@@ -261,18 +247,8 @@ namespace fr.ipmfrance.webcam
                                 }
                             }
                         }
-
-                        //if (needToSetVideoInput)
-                        //{
-                        //    needToSetVideoInput = false;
-                        //    if (isCrossbarAvailable.Value)
-                        //    {
-                        //        SetCurrentCrossbarInput(crossbar, crossbarVideoInput);
-                        //        crossbarVideoInput = GetCurrentCrossbarInput(crossbar);
-                        //    }
-                        //}
-
                     }
+
                     while (!stopEvent.WaitOne(100, false));
 
                     mediaControl.Stop();
@@ -294,8 +270,6 @@ namespace fr.ipmfrance.webcam
                 videoControl = null;
                 mediaEvent = null;
                 pinStillImage = null;
-            //    crossbar = null;
-
                 videoGrabberBase = null;
                 videoSampleGrabber = null;
 
@@ -331,110 +305,6 @@ namespace fr.ipmfrance.webcam
                 PlayingFinished(this, reasonToStop);
             }
         }
-
-        //private void DisplayPropertyPage(IntPtr parentWindow, object sourceObject)
-        //{
-        //    try
-        //    {
-        //        ISpecifyPropertyPages pPropPages = (ISpecifyPropertyPages)sourceObject;
-        //        CAUUID caGUID;
-        //        pPropPages.GetPages(out caGUID);
-        //        FilterInfo filterInfo = new FilterInfo(deviceMoniker);
-        //        Win32.OleCreatePropertyFrame(parentWindow, 0, 0, filterInfo.Name, 1, ref sourceObject, caGUID.cElems, caGUID.pElems, 0, 0, IntPtr.Zero);
-        //        Marshal.FreeCoTaskMem(caGUID.pElems);
-        //    }
-        //    catch
-        //    {
-        //    }
-        //}
-
-        //private VideoInput GetCurrentCrossbarInput(IAMCrossbar crossbar)
-        //{
-        //    VideoInput videoInput = VideoInput.Default;
-
-        //    int inPinsCount, outPinsCount;
-
-        //    if (crossbar.get_PinCounts(out outPinsCount, out inPinsCount) == 0)
-        //    {
-        //        int videoOutputPinIndex = -1;
-        //        int pinIndexRelated;
-        //        PhysicalConnectorType type;
-
-        //        for (int i = 0; i < outPinsCount; i++)
-        //        {
-        //            if (crossbar.get_CrossbarPinInfo(false, i, out pinIndexRelated, out type) != 0)
-        //                continue;
-
-        //            if (type == PhysicalConnectorType.VideoDecoder)
-        //            {
-        //                videoOutputPinIndex = i;
-        //                break;
-        //            }
-        //        }
-
-        //        if (videoOutputPinIndex != -1)
-        //        {
-        //            int videoInputPinIndex;
-
-        //            if (crossbar.get_IsRoutedTo(videoOutputPinIndex, out videoInputPinIndex) == 0)
-        //            {
-        //                PhysicalConnectorType inputType;
-
-        //                crossbar.get_CrossbarPinInfo(true, videoInputPinIndex, out pinIndexRelated, out inputType);
-
-        //                videoInput = new VideoInput(videoInputPinIndex, inputType);
-        //            }
-        //        }
-        //    }
-
-        //    return videoInput;
-        //}
-
-        //private void SetCurrentCrossbarInput(IAMCrossbar crossbar, VideoInput videoInput)
-        //{
-        //    if (videoInput.Type != PhysicalConnectorType.Default)
-        //    {
-        //        int inPinsCount, outPinsCount;
-
-        //        if (crossbar.get_PinCounts(out outPinsCount, out inPinsCount) == 0)
-        //        {
-        //            int videoOutputPinIndex = -1;
-        //            int videoInputPinIndex = -1;
-        //            int pinIndexRelated;
-        //            PhysicalConnectorType type;
-
-        //            for (int i = 0; i < outPinsCount; i++)
-        //            {
-        //                if (crossbar.get_CrossbarPinInfo(false, i, out pinIndexRelated, out type) != 0)
-        //                    continue;
-
-        //                if (type == PhysicalConnectorType.VideoDecoder)
-        //                {
-        //                    videoOutputPinIndex = i;
-        //                    break;
-        //                }
-        //            }
-
-        //            for (int i = 0; i < inPinsCount; i++)
-        //            {
-        //                if (crossbar.get_CrossbarPinInfo(true, i, out pinIndexRelated, out type) != 0)
-        //                    continue;
-
-        //                if ((type == videoInput.Type) && (i == videoInput.Index))
-        //                {
-        //                    videoInputPinIndex = i;
-        //                    break;
-        //                }
-        //            }
-
-        //            if ((videoInputPinIndex != -1) && (videoOutputPinIndex != -1) &&
-        //                 (crossbar.CanRoute(videoOutputPinIndex, videoInputPinIndex) == 0))
-        //            {
-        //                crossbar.Route(videoOutputPinIndex, videoInputPinIndex);
-        //            }
-        //        }
-        //    }
-        //}
 
         private void OnNewFrame(Bitmap image)
         {
