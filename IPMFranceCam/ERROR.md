e545964b96cd99e25e44af19261fa2d56a6e91c6
DaKeoZ <aurelien.farges@gmail.com>
2021-06-09 00:55:34 +0200

    Memory access errors

--- IPMFranceCam/ERROR.md
+++ IPMFranceCam/ERROR.md
@@ -1,1 +1,5 @@
 ﻿Exception levée à 0x00007FFF99FD0E7F dans obs64.exe : 0xC0000005 : Violation d'accès lors de la lecture de l'emplacement 0x0000000000000000
+
+Output OBS :
+ - Video path null pour la caméra virtuelle mais retourne le MonikerString de la vraie caméra quand sélectionnée
+ - Format XRGB pour la caméra virtuelle mais MJPEG pour la vraie caméra
--- IPMFranceCam/fr/ipmfrance/webcam/DesktopCapture.cs
+++ IPMFranceCam/fr/ipmfrance/webcam/DesktopCapture.cs
@@ -4,28 +4,27 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace fr.ipmfrance.webcam
 {
     public class DesktopCapture : IDisposable
     {
 
-        protected IntPtr m_hMemDC = IntPtr.Zero;
-        protected int m_nMaxWidth = 0;
-        protected int m_nMaxHeight = 0;
-        protected IntPtr m_hBitmap = IntPtr.Zero;
-        protected IntPtr handleDesktop = IntPtr.Zero;
-        protected int m_nWidth = CamConst.c_iDefaultWidth;
-        protected int m_nHeight = CamConst.c_iDefaultHeight;
-        private Bitmap imageBitmap;
-        private IPMWebcam videoSource;
-        private FilterInfo filter;
-        private object sync = new object();
-        private Bitmap currentFrame = null;
-        private Bitmap convertedFrame = null;
-        private string lastMessage = null;
-        private bool firstFrameNotProcessed = true;
-        private volatile bool requestedToStop = false;
+        public IntPtr m_hMemDC = IntPtr.Zero;
+        public int m_nMaxWidth = 0;
+        public int m_nMaxHeight = 0;
+        public IntPtr m_hBitmap = IntPtr.Zero;
+        public IntPtr handleDesktop = IntPtr.Zero;
+        public int m_nWidth = CamConst.c_iDefaultWidth;
+        public int m_nHeight = CamConst.c_iDefaultHeight;
+        public IPMWebcam videoSource;
+        public FilterInfo filter;
+        public Bitmap currentFrame = null;
+        public Bitmap convertedFrame = null;
+        public string lastMessage = null;
+        public bool firstFrameNotProcessed = true;
+        public volatile bool requestedToStop = false;
 
         public DesktopCapture()
         {
@@ -36,7 +35,7 @@ namespace fr.ipmfrance.webcam
             OpenVideoSource();
         }
 
-        private void OpenVideoSource()
+        public void OpenVideoSource()
         {
             File.AppendAllText(@"F:\out.txt", "OpenVideoSource\n");
             CloseCurrentVideoSource();
@@ -44,7 +43,7 @@ namespace fr.ipmfrance.webcam
             VideoSource.Start();
         }
 
-        private void CloseCurrentVideoSource()
+        public void CloseCurrentVideoSource()
         {
             File.AppendAllText(@"F:\out.txt", "CloseCurrentVideoSource\n");
 
@@ -82,13 +81,10 @@ namespace fr.ipmfrance.webcam
                     videoSource.PlayingFinished -= new PlayingFinishedEventHandler(videoSource_PlayingFinished);
                 }
 
-                lock (sync)
+                if (currentFrame != null)
                 {
-                    if (currentFrame != null)
-                    {
-                        currentFrame.Dispose();
-                        currentFrame = null;
-                    }
+                    currentFrame.Dispose();
+                    currentFrame = null;
                 }
 
                 videoSource = value;
@@ -110,7 +106,7 @@ namespace fr.ipmfrance.webcam
         public event NewFrameHandler NewFrame;
         public event PlayingFinishedEventHandler PlayingFinished;
 
-        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        public void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             File.AppendAllText(@"F:\out.txt", "videoSource_NewFrame\n");
             if (!requestedToStop)
@@ -122,65 +118,65 @@ namespace fr.ipmfrance.webcam
                 {
                     NewFrame(this, ref newFrame);
                 }
-
-                // now update current frame of the control
-                lock (sync)
+                // dispose previous frame
+                if (currentFrame != null)
                 {
-                    // dispose previous frame
-                    if (currentFrame != null)
-                    {
-                        currentFrame.Dispose();
-                        currentFrame = null;
-                    }
-                    if (convertedFrame != null)
-                    {
-                        convertedFrame.Dispose();
-                        convertedFrame = null;
-                    }
+                    currentFrame.Dispose();
+                    currentFrame = null;
+                }
+                if (convertedFrame != null)
+                {
+                    convertedFrame.Dispose();
+                    convertedFrame = null;
+                }
 
-                    currentFrame = newFrame;
-                    lastMessage = null;
+                currentFrame = newFrame;
+                lastMessage = null;
 
-                    // check if conversion is required to lower bpp rate
-                    if ((currentFrame.PixelFormat == PixelFormat.Format16bppGrayScale) ||
-                         (currentFrame.PixelFormat == PixelFormat.Format48bppRgb) ||
-                         (currentFrame.PixelFormat == PixelFormat.Format64bppArgb))
-                    {
-                        convertedFrame = webcam.Image.Convert16bppTo8bpp(currentFrame);
-                    }
+                // check if conversion is required to lower bpp rate
+                if ((currentFrame.PixelFormat == PixelFormat.Format16bppGrayScale) ||
+                        (currentFrame.PixelFormat == PixelFormat.Format48bppRgb) ||
+                        (currentFrame.PixelFormat == PixelFormat.Format64bppArgb))
+                {
+                    convertedFrame = webcam.Image.Convert16bppTo8bpp(currentFrame);
                 }
             }
         }
 
         // Error occured in video source
-        private void videoSource_VideoSourceError(object sender, VideoSourceErrorEventArgs eventArgs)
+        public void videoSource_VideoSourceError(object sender, VideoSourceErrorEventArgs eventArgs)
         {
             File.AppendAllText(@"F:\out.txt", "videoSource_VideoSourceError\n");
             lastMessage = eventArgs.Description;
         }
 
         // Video source has finished playing video
-        private void videoSource_PlayingFinished(object sender, ReasonToFinishPlaying reason)
+        public void videoSource_PlayingFinished(object sender, ReasonToFinishPlaying reason)
         {
             switch (reason)
             {
                 case ReasonToFinishPlaying.EndOfStreamReached:
+                    File.AppendAllText(@"F:\out.txt", reason.ToString() + "\n");
                     lastMessage = "Video has finished";
                     break;
 
                 case ReasonToFinishPlaying.StoppedByUser:
+                    File.AppendAllText(@"F:\out.txt", reason.ToString() + "\n");
                     lastMessage = "Video was stopped";
                     break;
 
                 case ReasonToFinishPlaying.DeviceLost:
+                    File.AppendAllText(@"F:\out.txt", reason.ToString() + "\n");
                     lastMessage = "Video device was unplugged";
                     break;
 
                 case ReasonToFinishPlaying.VideoSourceError:
+                    File.AppendAllText(@"F:\out.txt", reason.ToString() + "\n");
                     lastMessage = "Video has finished because of error in video source";
                     break;
 
                 default:
+                    File.AppendAllText(@"F:\out.txt", reason.ToString() + "\n");
                     lastMessage = "Video has finished for unknown reason";
                     break;
             }
@@ -202,7 +198,7 @@ namespace fr.ipmfrance.webcam
             m_nMaxHeight = Gdi32.GetDeviceCaps(handleDesktop, 10);
             m_hMemDC = Gdi32.CreateCompatibleDC(handleDesktop);
             // m_hBitmap = Gdi32.CreateCompatibleBitmap(handleDesktop, m_nWidth, Math.Abs(m_nHeight));
-            m_hBitmap = imageBitmap.GetHbitmap();
+            m_hBitmap = convertedFrame.GetHbitmap();
         }
 
         public void DeleteBitmap()
@@ -215,7 +211,7 @@ namespace fr.ipmfrance.webcam
             }
         }
 
-        private void SetTextColor(Color color)
+        public void SetTextColor(Color color)
         {
             File.AppendAllText(@"F:\out.txt", "SetTextColor\n");
             int rgb = (color.B & 0xFF) << 16 | (color.G & 0xFF) << 8 | color.R;
@@ -246,7 +242,7 @@ namespace fr.ipmfrance.webcam
             Gdi32.GetDIBits(m_hMemDC, m_hBitmap, 0, (uint)Math.Abs(m_nHeight), ptrSampleMediaBuffer, ref m_bmi, 0);
         }
 
-        private void WriteHello()
+        public void WriteHello()
         {
             string text = "Hello World !!!!!!!!!!";
 //            NativeTextRenderer nativeTextRenderer = new NativeTextRenderer(m_hMemDC);
